400ee48d912b1f3df6e60c1833ebdac5911a15de
Axel RINALDI <axel.rinaldi@wanadoo.fr>
2019-11-06 16:04:40 +0100

    Ajout du filtrage de la liste des stands

--- src/app/stands/stands.page.ts
+++ src/app/stands/stands.page.ts
@@ -15,11 +15,13 @@ export class StandsPage implements OnInit
   private numMin:number ;
   private numMax:number ;
   public stands:Array<{id:string}>;
+  public standsOriginalList:Array<{id:string}>;
   
 
   constructor(private route: Router, private sqlPrd:RemoteSqlProvider) 
   { 
     this.stands=[];
+    this.standsOriginalList = [] ;
     this.numMin = 0 ;
     this.numMax = 9999 ;
   }
@@ -36,7 +38,11 @@ export class StandsPage implements OnInit
 			"SELECT * FROM stand_18 WHERE id >= " + this.numMin + " AND id < " + this.numMax,
 			null,
 			this.stands
-		);  
+    ).then(()=>
+    {
+      // Enregistre une copie de la liste originale
+      this.standsOriginalList = this.stands ;
+    });  
   }
 
   onSelectStand( stand:{id:string} )
@@ -46,7 +52,22 @@ export class StandsPage implements OnInit
 
   onSearchStand( event )
   {
-
+    {
+      // Reset items back to all of the items
+      this.stands = this.standsOriginalList
+  
+      // set val to the value of the searchbar
+      let value = event.target.value;
+  
+      // if the value is an empty string don't filter the items
+      if (value && value.trim() != '') 
+      {
+        // Regénère la liste filtrée.
+        this.stands = this.stands.filter((stand) => 
+        {
+          return (stand.id.indexOf(value.toLowerCase()) > -1);
+        }) ;
+      }
+    }
   }
-
 }
