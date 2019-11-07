32100138b47e581831d1f90994ca4355b2f5ce51
Axel RINALDI <axel.rinaldi@wanadoo.fr>
2019-11-07 10:27:01 +0100

    Ajout de la liste des exposants

--- src/app/exposants/exposants.page.ts
+++ src/app/exposants/exposants.page.ts
@@ -1,18 +1,133 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, NavigationExtras } from '@angular/router' ;
+import { RemoteSqlProvider } from '../../providers/remotesql/remotesql';
+import { Marqueur } from '../components/plan/plan.component' ;
+
 @Component({
   selector: 'app-exposants',
   templateUrl: './exposants.page.html',
   styleUrls: ['./exposants.page.scss'],
 })
-export class ExposantsPage implements OnInit {
+export class ExposantsPage implements OnInit 
+{
 
-  constructor(private route: Router) { }
   accueilpage()
   {
     this.route.navigate(['']);
   }
-  ngOnInit() {
+
+  private recherche: RechercheExposantCriteres ;
+  private exposants: Array<any> ;
+  private marqueurs: Array<Marqueur> ;
+  private themes: Array<{id: string, libelle: string}> ;
+
+  constructor(
+    private route: Router,
+    private sqlPrd: RemoteSqlProvider ) 
+  {
+    this.recherche = new RechercheExposantCriteres() ;
+    this.exposants = [] ;
+    this.marqueurs = [] ;
+    this.themes = [] ;
   }
 
+  ngOnInit()
+  {
+    // Charge les thèmes
+    let sql = "select id, libelle from theme_18 order by libelle";
+    this.sqlPrd.select( sql, null, this.themes );
+
+  }
+
+  onRecherche()
+  {
+    this.exposants = [] ;
+    this.marqueurs = [] ;
+
+    if (this.recherche.libelle && this.recherche.themeId)
+    {
+      let libelle = this.recherche.libelle.toLocaleUpperCase();
+
+      let sql = "SELECT stand_18.id as idStand, exposant_18.id, exposant_18.nom, exposant_18.image "
+      sql += "FROM exposant_18 "
+      sql += "JOIN presenter_18 ON exposant_18.id = presenter_18.idExposant "
+      sql += "JOIN theme_18 ON presenter_18.idTheme = theme_18.id "
+      sql += "JOIN etresur_18 ON exposant_18.id = etresur_18.idExposant "
+      sql += "JOIN stand_18 ON etresur_18.idStand = stand_18.id "
+      sql += "WHERE theme_18.id = " + this.recherche.themeId + " AND exposant_18.nom LIKE '%" + libelle + "%' "
+      sql += " ORDER BY exposant_18.nom"
+
+      this.sqlPrd.select(sql, null, this.exposants).then((data) => {
+        this.exposants.forEach((e) => {
+          this.marqueurs.push(new Marqueur(e.idStand, e.nom))
+        })
+      })
+
+    }
+    else if( this.recherche.libelle )
+    {
+      let libelle = this.recherche.libelle.toLocaleUpperCase();
+
+      let sql = "SELECT stand_18.id as idStand, exposant_18.id, exposant_18.nom, exposant_18.image "
+      sql += "FROM exposant_18 "
+      sql += "JOIN etresur_18 ON exposant_18.id = etresur_18.idExposant "
+      sql += "JOIN stand_18 ON etresur_18.idStand = stand_18.id "
+      sql += "WHERE exposant_18.nom LIKE '%" + libelle + "%' "
+      sql += "ORDER BY exposant_18.nom"
+      
+      this.sqlPrd.select( sql, null, this.exposants ).then((data)=>
+      {
+        this.exposants.forEach( (e)=>
+        {
+          this.marqueurs.push( new Marqueur( e.idStand, e.nom) ) ;
+        }) ;
+      }) ;
+    }
+    else if( this.recherche.themeId )
+    {
+      let sql = "SELECT stand_18.id as idStand, exposant_18.id, exposant_18.nom, exposant_18.image "
+      sql += "FROM exposant_18 "
+      sql += "JOIN presenter_18 ON exposant_18.id = presenter_18.idExposant "
+      sql += "JOIN theme_18 ON presenter_18.idTheme = theme_18.id "
+      sql += "JOIN etresur_18 ON exposant_18.id = etresur_18.idExposant "
+      sql += "JOIN stand_18 ON etresur_18.idStand = stand_18.id "
+      sql += "WHERE theme_18.id = " + this.recherche.themeId
+      sql += " ORDER BY exposant_18.nom"
+
+      console.log(this.recherche.themeId)
+
+      this.sqlPrd.select( sql , null, this.exposants ).then((data)=>
+      {
+        this.exposants.forEach( (e)=>
+        {
+          this.marqueurs.push( new Marqueur( e.idStand, e.nom) ) ;
+        }) ;
+      }) ;      
+    }
+    else
+    {
+      let sql = "select idStand, id, nom "
+      sql += "from exposant_18 "
+      sql += "JOIN etresur_18 ON id = idExposant " 
+      sql += "order by nom"
+      this.sqlPrd.select( sql , null, this.exposants ) ;
+    }
+  }
+
+  onExposantClick( exposant ) 
+  {
+    //this.navCtrl.push( ExposantPage, {id: exposant.id} ) ;
+  }
+
+  onPlan()
+  {
+    //this.navCtrl.push( PlansPage, {marqueurs: this.marqueurs} )
+  }
+}
+
+
+export class RechercheExposantCriteres
+{
+  public libelle: string ;
+  public themeId: string ;
 }
--- src/app/pages/stand/stand.module.ts
+++ src/app/pages/stand/stand.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Routes, RouterModule } from '@angular/router';
@@ -6,6 +6,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 
 import { StandPage } from './stand.page';
+
 
 const routes: Routes = [
   {
@@ -21,6 +22,9 @@ const routes: Routes = [
     IonicModule,
     RouterModule.forChild(routes)
   ],
-  declarations: [StandPage]
+  declarations: [StandPage],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA
+  ]  
 })
 export class StandPageModule {}
