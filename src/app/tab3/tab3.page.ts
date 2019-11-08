c3f13106663e73cc529b7ad46cb45d9cb2409c31
Axel RINALDI <axel.rinaldi@wanadoo.fr>
2019-11-08 09:47:22 +0100

    Modification des appels de pages dans tab3

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -5,35 +5,12 @@ import { Router } from '@angular/router';
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
-export class Tab3Page {
 
-constructor(private route: Router) { }
-  standpage()
-  {
-    this.route.navigate(['/stands']);
-  }
-  themepage()
-  {
-      this.route.navigate(['/themes']);
-  }
-  rdvpage()
-  {
-         this.route.navigate(['/rdv']);
-  }
-  exposantpage()
-  {
-      this.route.navigate(['/exposants']);
-  }
-  livrepage()
-  {
-      this.route.navigate(['/livres']);
-  }
-  auteurpage()
-  {
-      this.route.navigate(['/auteurs']);
-  }
-  favorispage()
-  {
-      this.route.navigate(['/favoris']);
-  }
+export class Tab3Page 
+{
+
+    constructor(private route: Router) 
+    { 
+
+    }
 }
